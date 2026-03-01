3971acc89fc9666e6f21005f656e191052274005
Backlyne <spint990@live.fr>
2026-03-01 10:52:27 +0100

    Update ParadiseEnchancer.lua

--- ParadiseEnchancer.lua
+++ ParadiseEnchancer.lua
@@ -62,7 +62,7 @@ local Config = {
     AutoSell          = true,
     AutoMeteor        = true,
     RejoinOnGift9     = true,
-    SelectedCase      = "HAZARDOUS",
+    SelectedCase      = "HazardousCase",
     CaseQuantity      = 5,
     WildMode          = false, 
 }
@@ -475,7 +475,7 @@ task.spawn(function()
             if OpenCase("LIGHT", false, 5, true) then continue end
         end
         if Config.AutoTicketCase and GetTickets() >= 50 then
-            if OpenCase("HAZARDOUS", false, 5, false) then continue end
+            if OpenCase("HazardousCase", false, 5, false) then continue end
         end
 
         -- 5. Quest Battles
